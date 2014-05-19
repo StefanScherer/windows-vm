9a8c75baec1b66981f72e50a18f0a37298c0f085
Stefan Scherer <scherer_stefan@icloud.com>
2014-05-19 20:00:48 +0100

    added more vcloud patches

--- build-win2008r2-datacenter-cygwin-vcloud.bat
+++ build-win2008r2-datacenter-cygwin-vcloud.bat
@@ -1,2 +1,4 @@
 if not exist box\vcloud mkdir box\vcloud
-packer build --only=vmware-iso -var "iso_url=%WIN2008R2_X64%" win2008r2-datacenter-cygwin-vcloud.json  | addtime
+set ISO=http://roecloudsrv001/windows/win2008r2sp1_datacenter_en/SW_DVD5_Windows_Svr_DC_EE_SE_Web_2008_R2_64Bit_English_w_SP1_MLF_X17-22580.ISO
+set CHECKSUM=7e7e9425041b3328ccf723a0855c2bc4f462ec57
+packer build --only=vmware-iso -var "iso_url=%ISO%" -var "iso_checksum=%CHECKSUM%" win2008r2-datacenter-cygwin-vcloud.json  | addtime
